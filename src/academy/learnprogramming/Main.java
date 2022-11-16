30a1c6492f9185c7fcd31f6558ad7c5f730f2911
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-16 15:33:57 +0900

    Primitive Data Types: float and double

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("Primitive Data Types: float and double");
+
+
+
+    }
+}
